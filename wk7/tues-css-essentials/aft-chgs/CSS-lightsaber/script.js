e974773995e4a4d5c84ca9a118ff6395214c2eb6
Simon <simon.curran.89@gmail.com>
2020-05-12 15:26:46 +1000

    end of wk8 tues work

---
+++ wk7/tues-css-essentials/aft-chgs/CSS-lightsaber/script.js
@@ -0,0 +1,13 @@
+// var saberOn = document.getElementById("saberOn");
+// var saberOff = document.getElementById("saberOff");
+// var wholeSaber = document.getElementById("whole-saber");
+// var hilt = document.getElementById("hilt");
+
+// // var saberOn = $("#saberOn")[0];
+// // var saberOff = $("#saberOff")[0];
+// // saberOn.play();
+// // saberOff.play();
+
+// wholeSaber.mouseenter(function() {
+//   saberOn.play();
+// });
